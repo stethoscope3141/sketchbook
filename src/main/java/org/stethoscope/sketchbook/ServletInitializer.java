8f1064a4a6ec9690e82b92c3e25a1c8d506591dd
jwt1029 <jwt1029@midasin.com>
2021-08-17 16:41:27 +0900

    init

---
+++ src/main/java/org/stethoscope/sketchbook/ServletInitializer.java
@@ -0,0 +1,13 @@
+package org.stethoscope.sketchbook;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SketchbookApplication.class);
+    }
+
+}
---
+++ src/test/java/org/stethoscope/sketchbook/SketchbookApplicationTests.java
@@ -0,0 +1,13 @@
+package org.stethoscope.sketchbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SketchbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
